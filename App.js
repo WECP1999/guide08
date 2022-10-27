1ba71b4c63e1a786f1b0c73744ecd79a128d907c
Walter Corpeno <wcorpeno@applaudostudios.com>
2022-10-27 16:18:10 -0600

    :sparkles: finish guide

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import HomeScreen from "./HomeScreen";
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import { StyleSheet } from "react-native";
+import WebScreen from "./WebScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Web: {
+      screen: WebScreen,
+    },
+  },
+  { initialRouteName: "Home" }
+);
+export default createAppContainer(AppNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
